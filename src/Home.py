e848c2dd60d5ed8ad806e3cd99b295cdf21bacbf
Eric Melz <eric@emelz.com>
2025-04-15 11:02:25 -0700

    added a link for testing

--- src/Home.py
+++ src/Home.py
@@ -7,7 +7,9 @@ answer = st.text_input("English word")
 submit = st.button("submit")
 if submit:
     if answer == "but":
-        st.info("Correct!!")
+        st.success("Correct!!")
         st.balloons()
     else:
         st.error("Sorry!")
+        st.markdown("<https://otter.ai/s/kR-WbeUUT7asz52Hwv0DNQ?snpt=true>")
+
